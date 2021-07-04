db2b8ca90dcfdee71db7fcbcf6c5042e909c3183
Breno Estrella <brenogbestrella@gmail.com>
2021-07-04 11:43:13 -0300

    Commit com ajuste da página Home para React

--- Backend/database/config/config.js
+++ Backend/database/config/config.js
@@ -1,10 +1,12 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "Thi@go_2020",
+    "password": "Coringa19@",
     "database": "projeto_integrador",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
 };
 
+
+Math.
--- Backend/database/models/Ordem.js
+++ Backend/database/models/Ordem.js
@@ -41,5 +41,4 @@ module.exports = (sequelize, DataTypes) => {
   //inserir id_usuario
 
   return Ordem;
-}
 };
